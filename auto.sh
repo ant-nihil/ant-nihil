1e63384570f7004bcbc526adac0f55e6211c6854
ant-nihil <90468138+ant-nihil@users.noreply.github.com>
2021-09-17 10:56:26 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -11,8 +11,8 @@ wget -P raspberrypi-kernel-headers_1.20210303-1_arm64 http://archive.raspberrypi
 mkdir raspberrypi-kernel_1.20210303-1_arm64
 wget -P raspberrypi-kernel_1.20210303-1_arm64 http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-kernel_1.20210303-1_arm64.deb
 
-echo "qwer1234" | -S sudo dpkg -i ~/raspberrypi-kernel-headers_1.20210303-1_arm64/raspberrypi-kernel-headers_1.20210303-1_arm64.deb
-echo "qwer1234" | -S sudo dpkg -i ~/raspberrypi-kernel_1.20210303-1_arm64/raspberrypi-kernel_1.20210303-1_arm64.deb
+echo "qwer1234" | sudo -S dpkg -i ./raspberrypi-kernel-headers_1.20210303-1_arm64/raspberrypi-kernel-headers_1.20210303-1_arm64.deb
+echo "qwer1234" | sudo -S dpkg -i ./raspberrypi-kernel_1.20210303-1_arm64/raspberrypi-kernel_1.20210303-1_arm64.deb
 
 #description:copy bcm2711-rpi-4-b.dtb to the specified directory
 echo "qwer1234" | -S sudo cp /boot/bcm2711-rpi-4-b.dtb /etc/flash-kernel/dtbs/
