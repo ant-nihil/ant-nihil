5a437171081c96c17dd1b43fe3ce3cd94fe7c5f9
ant-nihil <90468138+ant-nihil@users.noreply.github.com>
2021-09-17 12:55:00 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -21,3 +21,14 @@ echo "12345678" | sudo -S cp /boot/bcm2711-rpi-4-b.dtb /etc/flash-kernel/dtbs/
 rm -r -f raspberrypi-kernel-headers_1.20210303-1_arm64 raspberrypi-kernel_1.20210303-1_arm64
 
 
+#description:change the running kernel to 5.10.17
+cd /boot/
+
+echo "12345678" | sudo -S cp vmlinuz-5.4.0-1042-raspi vmlinuz-5.4.0-1042-raspi.bak
+echo "12345678" | sudo -S cp kernel8.img vmlinuz-5.4.0-1042-rasp
+echo "12345678" | sudo -S cp bcm2711-rpi-cm4.dtb dtbs/5.10.17-v8+/./
+echo "12345678" | sudo -S rm dtb-5.10.17-v8+
+echo "12345678" | sudo -S ln -s dtbs/5.10.17-v8+/./bcm2711-rpi-cm4.dtb dtb-5.10.17-v8+
+echo "12345678" | sudo -S update-initramfs -u
+sync
+echo "12345678" | reboot
