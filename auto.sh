768f34443355142a1a0267ae12e76f705ff77328
ant-nihil <90468138+ant-nihil@users.noreply.github.com>
2021-09-17 12:34:01 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "This is a test code for auto start"
-password="qwer1234"
+password="12345678"
 
 #description:download the .deb file into ~/raspberrypi-kernel-headers_1.20210303-1_arm64/
 mkdir raspberrypi-kernel-headers_1.20210303-1_arm64
@@ -11,11 +11,11 @@ wget -P raspberrypi-kernel-headers_1.20210303-1_arm64 http://archive.raspberrypi
 mkdir raspberrypi-kernel_1.20210303-1_arm64
 wget -P raspberrypi-kernel_1.20210303-1_arm64 http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-kernel_1.20210303-1_arm64.deb
 
-echo "qwer1234" | sudo -S dpkg -i ./raspberrypi-kernel-headers_1.20210303-1_arm64/raspberrypi-kernel-headers_1.20210303-1_arm64.deb
-echo "qwer1234" | sudo -S dpkg -i ./raspberrypi-kernel_1.20210303-1_arm64/raspberrypi-kernel_1.20210303-1_arm64.deb
+echo "12345678" | sudo -S dpkg -i ./raspberrypi-kernel-headers_1.20210303-1_arm64/raspberrypi-kernel-headers_1.20210303-1_arm64.deb
+echo "12345678" | sudo -S dpkg -i ./raspberrypi-kernel_1.20210303-1_arm64/raspberrypi-kernel_1.20210303-1_arm64.deb
 
 #description:copy bcm2711-rpi-4-b.dtb to the specified directory
-echo "qwer1234" | sudo -S cp /boot/bcm2711-rpi-4-b.dtb /etc/flash-kernel/dtbs/
+echo "12345678" | sudo -S cp /boot/bcm2711-rpi-4-b.dtb /etc/flash-kernel/dtbs/
 
 #description:delate the two temporary directories
 rm -r -f raspberrypi-kernel-headers_1.20210303-1_arm64 raspberrypi-kernel_1.20210303-1_arm64
