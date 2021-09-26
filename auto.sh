8793d0b997e854a6919c73aa62a99c242a0dabca
ant-nihil <90468138+ant-nihil@users.noreply.github.com>
2021-09-26 15:57:22 +0800

    delete fix_desktop,now this shell script can only switch to the latest kernel version

--- auto.sh
+++ auto.sh
@@ -24,108 +24,74 @@ function get_kernel_version() {
     _VER_RUN=$(dd if=$ZIMAGE obs=64K ibs=4 skip=$(( IMG_OFFSET / 4)) 2>/dev/null | zcat | grep -a -m1 "Linux version" | strings | awk '{ print $3; }')
   }
   echo "$_VER_RUN"
-  
+
   return 0
 }
 
 function download_install_debpkg() {
   local version-kernel-headers version-kernel
   mkdir temporary_dir
-  
-  wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/ -O rpi-kernel-version-text 
-  
+
+  wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/ -O rpi-kernel-version-text
+
   version_kernel_headers=`cat rpi-kernel-version-text  | grep raspberrypi-kernel-headers | grep arm64 | awk 'END { print }' | awk '{print $6}' | awk -F '\"' '{print $2} '`
   version_kernel=`cat rpi-kernel-version-text  | grep raspberrypi-kernel | grep arm64 | awk 'END { print }' | awk '{print $6}' | awk -F '\"' '{print $2} '`
 
   wget -P temporary_dir $url$version_kernel_headers
   wget -P temporary_dir $url$version_kernel
-  
+
   sudo dpkg -i temporary_dir/$version_kernel_headers
   sudo dpkg -i temporary_dir/$version_kernel
-  
+
   #copy bcm2711-rpi-4-b.dtb to the specified directory,then install again
   sudo cp /boot/bcm2711-rpi-4-b.dtb /etc/flash-kernel/dtbs/
   sudo dpkg -i temporary_dir/$version_kernel
-  
+
   rm -r -f temporary_dir
 }
 
-#fix desktop
-function fix_desktop () {
-  
-  local num=1
-  #download until git fetch or 2000
-  while [ $num -le 2000 ]; do
-    git clone https://github.com/ant-nihil/seeed-linux-dtoverlays
-    if [ $? -ne 0 ]; then
-        num=$(($num+1))
-    else
-        break
-    fi
-  done
-  
-  cd seeed-linux-dtoverlays
-  
-  make all_rpi
-  sudo make install_rpi
-  
-  sudo echo "#-------------------------------------------
-dtoverlay=vc4-fkms-v3d
-enable_uart=1
-dtoverlay=dwc2,dr_mode=host
-dtparam=ant2
-disable_splash=1
-ignore_lcd=1
-dtoverlay=vc4-kms-v3d-pi4
-dtoverlay=i2c3,pins_4_5
-gpio=13=pu
-dtoverlay=reTerminal,tp_rotate=1
-#------------------------------------------" >> /boot/firmware/config.txt
-  cp /boot/overlays/reTerminal.dtbo /boot/firmware/overlays/reTerminal.dtbo
-  
-  make all_rpi
-  cd /boot/firmware/overlays/
-  sudo cp vc4-kms-v3d-pi4.dtbo vc4-kms-v3d-pi4.dtbo.bak
-  cp /home/ubuntu/seeed-linux-dtoverlays/overlays/rpi/reTerminal-overlay.dts /boot/firmware/overlays/vc4-kms-v3d-pi4.dtbo
-  
-  sudo apt -y --force-yes install ubuntu-desktop
-}
 
 #change the running kernel to 5.10.
 function install_kernel() {
   local vmlinuz kernel_name
-  
+
   sudo apt install make
   sudo apt -y --force-yes install build-essential
   download_install_debpkg
-  
+
   cd /boot/
   get_kernel_version
   vmlinuz="vmlinuz-$_VER_RUN"
+
   sudo cp $vmlinuz $vmlinuz.bak
   sudo cp kernel8.img $vmlinuz
-  
-  kernel_name=`find /boot/dtbs/ | grep + | awk 'END {print}' | awk -F "\/" '{print $4}'`
-  
-  sudo cp bcm2711-rpi-cm4.dtb dtbs/$kernel_name/./
-  sudo rm dtb-$kernel_name
-  sudo ln -s dtbs/$kernel_name/./bcm2711-rpi-cm4.dtb dtb-$kernel_name
+  kernel_name=`ls -1t /lib/modules/ | sed -n '1p'`
+
   sudo update-initramfs -u
+
+  sudo cp /boot/firmware/bcm2711-rpi-cm4.dtb /boot/firmware/bcm2711-rpi-cm4.dtb.bak
+  sudo cp /boot/bcm2711-rpi-cm4.dtb /boot/firmware/
+  sudo cp bcm2711-rpi-cm4.dtb dtbs/$_VER_RUN/./
+  sudo rm dtb
+  sudo ln -s dtbs/$_VER_RUN/./bcm2711-rpi-cm4.dtb dtb
+  sudo rm dtb-$_VER_RUN
+  sudo ln -s dtbs/$_VER_RUN/./bcm2711-rpi-cm4.dtb dtb-$_VER_RUN
+  sudo cp bcm2711-rpi-cm4.dtb dtbs/$kernel_name/./
+  sudo rm dtb-$_VER_RUN
+  ln -s dtbs/$kernel_name/./bcm2711-rpi-cm4.dtb dtb-$kernel_name
   sync
-  
+
   return 0
 }
 
 function install() {
-  
+
   install_kernel
-  
-  fix_desktop
-  
+
+
   echo "------------------------------------------------------"
   echo "Please reboot your device to apply all settings"
   echo "Enjoy!"
   echo "------------------------------------------------------"
 }
 install
-
